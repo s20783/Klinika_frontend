9c34342bd3ddca9ab43fb3199d7ffaa8f5b115f7
Michał Ostrowski <s20783@pjwstk.edu.pl>
2022-06-03 14:42:35 +0200

    Dodanie listy weterynarzy i walidacji pola w formularzu umawiania wizyty

---
+++ src/components/weterynarz/WeterynarzList.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {useNavigate} from "react-router";
+import {getWeterynarzList} from "../../api/WeterynarzApiCalls";
+import WeterynarzListTable from "./WeterynarzListTable";
+
+class WeterynarzList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: '',
+            isLoaded: false,
+            weterynarze: [],
+            notice: ''
+        }
+    }
+
+    componentDidMount() {
+        const {navigate} = this.props;
+        getWeterynarzList()
+            .then(res => {
+                console.log(res.status)
+                if (res.status === 401) {
+                    console.log('Potrzebny aktualny access token')
+                    navigate("/", {replace: true});
+                }
+                return res.json()
+            })
+            .then(
+                (data) => {
+                    this.setState({
+                        isLoaded: true,
+                        weterynarze: data
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const {error, isLoaded, weterynarze} = this.state
+        let content;
+
+        if (error) {
+            content = <p>Błąd: {error.message}</p>
+        } else if (!isLoaded) {
+            content = <p>Ładowanie...</p>
+        } else {
+            content = <WeterynarzListTable weterynarze={weterynarze}/>
+        }
+
+        return (
+            <main>
+                <section className="bg-gray-100 border-b  ">
+                    <div className="container w-full max-w-5xl  mx-auto px-2 py-8">
+                        <div id='recipients' className="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
+                            <h2 className="mt-6 w-full my-2 mb-6 text-5xl font-black leading-tight text-center text-gray-800">
+                                Weterynarze</h2>
+                            {content}
+                        </div>
+                    </div>
+                </section>
+            </main>
+        )
+    }
+}
+
+const withNavigate = Component => props => {
+    const navigate = useNavigate();
+    return <Component {...props} navigate={navigate}/>;
+};
+
+export default withNavigate(WeterynarzList);
