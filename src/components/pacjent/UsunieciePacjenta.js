4b31f3d794a24f9330a2240b763f7448a2c82ab3
s20783 <s20783@pjwstk.edu.pl>
2022-10-25 17:20:05 +0200

    usunięcie pacjenta

--- src/components/pacjent/UsunieciePacjenta.js
+++ src/components/pacjent/UsunieciePacjenta.js
@@ -1,84 +1,67 @@
 import React from "react";
 import {deletePacjent} from "../../api/PacjentApiCalls";
-import DodaniePacjentaForm from "./DodaniePacjentaForm";
-import formMode from "../helpers/FormMode";
-import {useParams,useNavigate} from "react-router";
+import {useParams, useNavigate} from "react-router";
 import {Link} from "react-router-dom";
 import {withTranslation} from "react-i18next";
 
 class UsunieciePacjenta extends React.Component {
     constructor(props) {
-            super(props);
-            const paramsIdPacjent = this.props.params.idPacjent
+        super(props);
+        const paramsIdPacjent = this.props.params.idPacjent
 
-            this.state = {
-                idPacjent: paramsIdPacjent,
-                error: '',
-                isLoaded: false,
-                notice: '',
-            }
+        this.state = {
+            idPacjent: paramsIdPacjent,
+            error: '',
+            isLoaded: false,
+            notice: '',
+        }
     }
 
-     deletePacjent = (idPacjent) => {
-            const {navigate} = this.props;
-            let response;
+    removePacjent = (idPacjent) => {
+        const {navigate} = this.props;
+        //let response;
+        deletePacjent(idPacjent)
+            .then(res => {
+                console.log(res)
+                if (res.status === 204) {
+                    console.log(res.status)
+                    navigate("/pacjenci", {replace: true});
 
-                deletePacjent(idPacjent)
-                    .then(res => {
-                        response = res
-                        return res.json()
-                        console.log(res)
-                    })
-                    .then(
-                        (data) => {
-                        console.log(data)
-                            if (response.status === 200 ) {
-                                console.log(response.status)
-                                navigate("/pacjenci", {replace: true});
+                } else if (res.status === 401) {
+                    console.log(res)
 
-                            } else if (response.status === 401) {
-                                console.log(data)
-                                this.setState({
-                                    message: data.message
-                                })
-                            } else {
-                                console.log(data)
-                                this.setState({
-                                    message: data.message
-                                })
-                            }
-                        },
-                        (error) => {
-                            this.setState({
-                                error: error
-                            })
-                        })
-        }
+                } else {
+                    console.log(res)
+
+                }
+            })
+    }
 
     render() {
-        const {error, isLoaded, idPacjent} = this.state
+        const {error, idPacjent} = this.state
         const {t} = this.props;
 
-        return(
-          <body class="bg-gray-200 flex items-center justify-center h-screen">
+        return (
+            <body class="bg-gray-200 flex items-center justify-center h-screen">
 
             <div class="modal-overlay absolute w-full h-full bg-gray-500 opacity-50"></div>
             <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded-lg shadow-lg z-50 overflow-y-auto">
 
-              <div class="modal-content py-9 px-5">
+                <div class="modal-content py-9 px-5">
+                    <p class="text-4xl mb-2 text-center font-bold">{t('pacjent.deletingPatient')}</p>
+                    <img src="/images/znakZapytaniaPies.png"/>
 
-                <p class="text-4xl mb-2 text-center font-bold">{t('pacjent.deletingPatient')}</p>
-                <img src="/images/znakZapytaniaPies.png"/>
-
-                <div class="flex justify-end pt-2">
-                <Link to="/pacjenci">
-                  <button  class="px-4 bg-transparent p-3 rounded-lg text-blue-400 hover:bg-gray-100 hover:text-blue-400 mr-2">{t('button.back')}</button></Link>
-                  <button  onClick={() => deletePacjent(idPacjent)} class=" px-4 bg-blue-400 p-3 rounded-lg text-white hover:bg-blue-400">{t('pacjent.deletePatient')}</button>
+                    <div class="flex justify-end pt-2">
+                        <Link to="/pacjenci">
+                            <button
+                                class="px-4 bg-transparent p-3 rounded-lg text-blue-400 hover:bg-gray-100 hover:text-blue-400 mr-2">{t('button.back')}</button>
+                        </Link>
+                        <button onClick={() => this.removePacjent(idPacjent)}
+                                class=" px-4 bg-blue-400 p-3 rounded-lg text-white hover:bg-blue-400">{t('pacjent.deletePatient')}</button>
+                    </div>
                 </div>
-
-              </div>
             </div>
-          </body>
+            </body>
         )
     }
 }
@@ -99,4 +82,4 @@ const withRouter = WrappedComponent => props => {
     );
 };
 
-export default withTranslation() (withRouter(withNavigate(UsunieciePacjenta)));
+export default withTranslation()(withRouter(withNavigate(UsunieciePacjenta)));
