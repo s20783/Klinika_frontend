abd36f0335f360beb7ab22b15891d83a060a72a4
s20783 <s20783@pjwstk.edu.pl>
2022-11-26 13:47:47 +0100

    harmonogram

--- src/components/harmonogram/Harmonogram.js
+++ src/components/harmonogram/Harmonogram.js
@@ -12,22 +12,21 @@ function Harmonogram(props) {
     console.log(harmonogram)
 
     return (
-
         <div>
-            <h2 className="mt-6 w-full my-2 mb-6 underline  text-3xl font-bold leading-tight text-left text-gray-500">
-                {getFormattedDate1(start)}<span className="mx-3  "> - </span>{getFormattedDate1(end)}</h2>
+            <h2 className="mt-6 w-full my-2 mb-6 underline text-center text-3xl font-bold leading-tight text-gray-600">
+                {getFormattedDate1(start)}<span className="mx-3"> - </span>{getFormattedDate1(end)}</h2>
             <table
                 className="w-full text-center flex flex-wrap text-gray-700 dark:text-gray-400">
                 <tr></tr>
                 <tr>
-                    <th className=" text-l mb-6 text-center flex flex-wrap mx-20 text-gray-700 uppercase underline decoration-double dark:bg-gray-700 dark:text-gray-400">
+                    <th className=" text-xl mb-6 text-center flex flex-wrap mx-20 text-gray-700 uppercase underline decoration-double dark:bg-gray-700 dark:text-gray-400">
                         {t("harmonogram.weekdays.pon")}</th>
                     {harmonogram.map(x => (
                         (x.Dzien === 1) &&
                         <td className="text-center w-full flex flex-wrap my-2 ">
                             <div className={(x.CzyZajete === false)
-                                ? ' border-l-4 border-green-300 mx-2 w-full' :
-                                ' border-l-4 border-red-300 mx-2 w-full'
+                                ? ' border-l-4 border-green-400 mx-2 w-full' :
+                                ' border-l-4 border-red-400 mx-2 w-full'
                             }>
                                 <div className="w-full">
                                             <span className={(x.Weterynarz === null || weterynarz === null)
@@ -49,8 +48,8 @@ function Harmonogram(props) {
                         (x.Dzien === 2) &&
                         <td className="text-center w-full flex flex-wrap my-2 ">
                             <div className={(x.CzyZajete === false)
-                                ? ' border-l-4 border-green-300 mx-2 w-full' :
-                                ' border-l-4 border-red-300 mx-2 w-full'
+                                ? ' border-l-4 border-green-400 mx-2 w-full' :
+                                ' border-l-4 border-red-400 mx-2 w-full'
                             }>
                                 <div className="w-full">
                                             <span className={(x.Weterynarz === null || weterynarz === null)
@@ -72,8 +71,8 @@ function Harmonogram(props) {
                         (x.Dzien === 3) &&
                         <td className="text-center w-full flex flex-wrap my-2 ">
                             <div className={(x.CzyZajete === false)
-                                ? ' border-l-4 border-green-300 mx-2 w-full' :
-                                ' border-l-4 border-red-300 mx-2 w-full'
+                                ? ' border-l-4 border-green-400 mx-2 w-full' :
+                                ' border-l-4 border-red-400 mx-2 w-full'
                             }>
                                 <div className="w-full">
                                             <span className={(x.Weterynarz === null || weterynarz === null)
@@ -95,8 +94,8 @@ function Harmonogram(props) {
                         (x.Dzien === 4) &&
                         <td className="text-center w-full flex flex-wrap my-2 ">
                             <div className={(x.CzyZajete === false)
-                                ? ' border-l-4 border-green-300 mx-2 w-full' :
-                                ' border-l-4 border-red-300 mx-2 w-full'
+                                ? ' border-l-4 border-green-400 mx-2 w-full' :
+                                ' border-l-4 border-red-400 mx-2 w-full'
                             }>
                                 <div className="w-full">
                                             <span className={(x.Weterynarz === null || weterynarz === null)
@@ -118,8 +117,8 @@ function Harmonogram(props) {
                         (x.Dzien === 5) &&
                         <td className="text-center w-full flex flex-wrap my-2 ">
                             <div className={(x.CzyZajete === false)
-                                ? ' border-l-4 border-green-300 mx-2 w-full' :
-                                ' border-l-4 border-red-300 mx-2 w-full'
+                                ? ' border-l-4 border-green-400 mx-2 w-full' :
+                                ' border-l-4 border-red-400 mx-2 w-full'
                             }>
                                 <div className="w-full">
                                             <span className={(x.Weterynarz === null || weterynarz === null)
@@ -138,11 +137,11 @@ function Harmonogram(props) {
                     <th className=" text-xl mb-6 text-center flex flex-wrap mx-20 text-gray-700 uppercase underline decoration-double dark:bg-gray-700 dark:text-gray-400">
                         {t("harmonogram.weekdays.sob")}</th>
                     {harmonogram.map(x => (
-                        (x.Dzien === 1) &&
+                        (x.Dzien === 6) &&
                         <td className="text-center w-full flex flex-wrap my-2 ">
                             <div className={(x.CzyZajete === false)
-                                ? ' border-l-4 border-green-300 mx-2 w-full' :
-                                ' border-l-4 border-red-300 mx-2 w-full'
+                                ? ' border-l-4 border-green-400 mx-2 w-full' :
+                                ' border-l-4 border-red-400 mx-2 w-full'
                             }>
                                 <div className="w-full">
                                             <span className={(x.Weterynarz === null || weterynarz === null)
